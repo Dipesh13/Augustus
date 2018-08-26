4d6e9fbd5999b08ea422a1d791a175f00bdfe534
Dipesh13 <mangwani.dipesh@gmail.com>
2018-08-27 04:04:58 +0530

    evaluate on unseen data

---
+++ c1/helper-functions/eval.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy import ndimage
+from predict import predict
+from model import d
+from load_dataset import classes
+
+my_image = "test.jpg"
+num_px = 64
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px, num_px)).reshape((1, num_px * num_px * 3)).T
+my_predicted_image = predict(d["w"], d["b"], my_image)
+
+plt.imshow(image)
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
