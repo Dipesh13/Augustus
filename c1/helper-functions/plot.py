223c8b36ea0dd1b6a3ae69bdc66e936329dd2038
Dipesh13 <mangwani.dipesh@gmail.com>
2018-08-27 04:10:27 +0530

    cost vs iterations plot

---
+++ c1/helper-functions/plot.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from model import d
+
+# Plot learning curve (with costs)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
